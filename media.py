84d204456e91707ccee5add2ef3f4607d6661c3a
Rankesh <Rankesh@users.noreply.github.com>
2017-07-08 18:27:19 +0530

    Code Update as per PEP8

--- media.py
+++ media.py
@@ -1,18 +1,22 @@
 import webbrowser
 
+
 class Movie():
 
     """ This class provides a way to store movie related information"""
-    
-    VALID_RATINGS = [ "G", "PG", "PG-13", "R"]
 
-    #Movie constructor
-    def __init__ (self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
+    # Movie constructor
+    def __init__(
+                    self, movie_title, movie_storyline,
+                    poster_image, trailer_youtube
+                ):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    #function to play youtube trailer
+    # Function to play youtube trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
